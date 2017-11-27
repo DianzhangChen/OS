e9a603950e689542e351ff7a0cea97597acab5f7
Hicodechar <chendianzhang@sjtu.edu.cn>
2017-11-27 18:28:31 +0800

    change pmap.c

--- jos-2017-spring/kern/pmap.c
+++ jos-2017-spring/kern/pmap.c
@@ -377,30 +377,11 @@ pgdir_walk(pde_t *pgdir, const void *va, int create)
 	//my note:
 	//pgdir is a *virtual addr*;
 	//the return of pgdir_walk is a *virtual addr* pointer to the sceondery page table entry;
-//	pde_t *pde = NULL;
-//	pte_t *pgtable = NULL;
-//
-//	struct Page *pp;
-//	pde = &pgdir[PDX(va)];
-//	if(*pde & PTE_P)
-//	{
-//		pgtable = (KADDR(PTE_ADDR(*pde)));
-//	}
-//	else
-//	{
-//		if(!create || !(pp=page_alloc(ALLOC_ZERO)) || !(pgtable = (pte_t *)page2kva(pp))){
-//			return NULL;
-//		}
-//		pp->pp_ref++;
-//		*pde = PADDR(pgtable) | PTE_P | PTE_W | PTE_U;
-//	}
-//	return &pgtable[PTX(va)];
 	pde_t * pde;
 	pte_t * pt;
 	pde = pgdir + PDX(va);
-	cprintf("the pgdir is: %x\n, and the *pgdir is :%x\n", pgdir, *pgdir);
-
-	cprintf("the pde is: %x\n, and the *pde is :%x\n", pde, *pde);
+	cprintf("the pgdir is: %x, and the *pgdir is :%x\n", pgdir, *pgdir);
+	cprintf("the pde is: %x, and the *pde is :%x\n", pde, *pde);
 
 	if((*pde) & PTE_P){
 	  pt = KADDR(PTE_ADDR(*pde));
@@ -417,7 +398,7 @@ pgdir_walk(pde_t *pgdir, const void *va, int create)
 		}
 		else return NULL;
 	}
-	cprintf("the pt is : %x\n", pt);
+	cprintf("the pt is : %x, and the *pt is %x\n", pt, *pt);
 	return pt+PTX(va);
 }
 
