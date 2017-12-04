de8085911c671d8037bbc2e55783f80eb6c02929
Hicodechar <chendianzhang@sjtu.edu.cn>
2017-12-05 06:45:50 +0800

    complete lab3's exercise 10

--- jos-2017-spring/kern/pmap.c
+++ jos-2017-spring/kern/pmap.c
@@ -657,7 +657,21 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-
+	uintptr_t va_start = (uintptr_t)va;
+	uintptr_t va_end = (uintptr_t)va+len;
+	uintptr_t va_p;
+	perm |= PTE_P;
+	for(va_p=va_start; va_p<va_end; va_p++){
+		if(va_p >= ULIM){
+			user_mem_check_addr = va_p;
+			return -E_FAULT;
+		}
+		pte_t * pte = pgdir_walk(env->env_pgdir, (void *)va_p, 0);
+		if(((*pte)&perm)!=perm || pte==NULL){
+			user_mem_check_addr = va_p;
+			return -E_FAULT;
+		}
+	}
 	return 0;
 }
 
